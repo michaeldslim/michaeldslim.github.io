25897e4186a716453fc884a65863872872177ad6
Mike Lim <michaelds.lim@gmail.com>
2025-07-13 21:22:17 -0500

    feat: initialize project with Jekyll theme and core dependencies

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "github.io Pages",
+        "excerpt":"GitHub Pages are public web hosted for free through GitHub. GitHub users can create and host both personal websites and websites related to specific GitHub projects. Pages lets you do the same things as GitHub, but if the repository is named a certain way and files inside it are HTML...","categories": ["Blog"],
+        "tags": ["blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser": null
+      },{
+        "title": "Find most frequent item of an array",
+        "excerpt":"Find most frequent item of an array with Javascript When I interviewed a while ago, I was asked the following quiz that needs to find out the most frequent item of an array. It’s been years that I interview to get this type of question so I was freak out....","categories": ["IT"],
+        "tags": ["javascript","code"],
+        "url": "http://localhost:4000/it/array-post/",
+        "teaser": null
+      },{
+        "title": "Install Jekyll and Run",
+        "excerpt":"1.Install Ruby first brew install ruby then add ruby path to .bashrc or .zshrc 2.Install Jekyll and bundler as well gem install -n /usr/local/bin jekyll bundler 3.Create a page and run to check jekyll new helloBlog cd helloBlog bundle exec jekyll serve then go to the preview address in the...","categories": ["Blog"],
+        "tags": ["jekyll","ruby"],
+        "url": "http://localhost:4000/blog/jekyll-post/",
+        "teaser": null
+      },{
+        "title": "React.js - Props and State",
+        "excerpt":"What are props? Props is for properties and they are used to pass data between React components. React’s data flow between components is unidirectional (from parent to child only). class Parent extends Component { render() { return ( &lt;Child name=\"John Doe\" /&gt; ); } } const Child = (props) =&gt;...","categories": ["Blog"],
+        "tags": ["react","props","state"],
+        "url": "http://localhost:4000/blog/props-post/",
+        "teaser": null
+      },{
+        "title": "Install Python Django",
+        "excerpt":"Install python django (Mac OS + Python3) pip install pipenv next step: create a django project folder (for example, Django) mkdir Django cd Django pipenv install django==2.2 pipenv shell django-admin startproject django_project . python manage.py runserver (to see a welcome page) ctrl+c to exit next step: create an app python...","categories": ["Blog"],
+        "tags": ["django"],
+        "url": "http://localhost:4000/blog/django-post/",
+        "teaser": null
+      }]
